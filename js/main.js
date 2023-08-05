1f366bf27d4c687fe4155c46f76d2a258a35d03f
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-05 17:16:26 +0900

    画面クリック時の表示（下の項目に情報を表示させる方法）

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 'use strict';
 
 {
+  const dts = document.querySelectorAll('dt');
 
-  
+  dts.forEach(dt => {
+    dt.addEventListener('click', () => {
+      dt.parentNode.classList.toggle('appear');
+    })
+  }
+    )
+
 }
 
