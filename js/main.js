d2a91fd6c6b8cdd1a931f4a4b286b1fcc760ccd4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-08-05 17:33:54 +0900

    1つを開いたら１つを閉じる動作

--- js/main.js
+++ js/main.js
@@ -6,6 +6,14 @@
   dts.forEach(dt => {
     dt.addEventListener('click', () => {
       dt.parentNode.classList.toggle('appear');
+
+      dts.forEach(el => {
+
+        if (dt !== el) {
+          el.parentNode.classList.remove('appear');
+        }
+
+      });
     })
   }
     )
